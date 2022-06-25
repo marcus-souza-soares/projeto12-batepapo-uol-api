8a3a5107dcddcc3f9a4a0d81de2a437d12b9b0fb
Marcus <marcusadrenaline@gmail.com>
2022-06-25 12:07:19 -0300

    post de menssagens implementado

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ const userSchema = joi.object({
     name: joi.string().required()
 })
 
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
+})
+
+//REQUISIÇÕES
+
 server.get("/participants", async (req, res) => {
     try {
         const usuarios = await db.collection('users').find().toArray();
@@ -34,16 +42,15 @@ server.get("/participants", async (req, res) => {
 
 server.post("/participants", async (req, res) => {
     const user = req.body;
-
-    const validation = userSchema.validate(req.body, { abortEarly: true });
-    if(validation.error){
+    const validation = userSchema.validate(user, { abortEarly: true });
+    if (validation.error) {
         console.log(validation.error);
         return sendStatus(422);
     }
 
-    const  verificaUser = await db.collection('users').findOne(user);
+    const verificaUser = await db.collection('users').findOne(user);
     console.log(verificaUser)
-    //CONDIÇÃO PARA NÃO 
+    //CONDIÇÃO PARA NÃO LOGAR COM UM USER DE MESMO NOME
     if (verificaUser) {
         res.sendStatus(409);
         return;
@@ -53,10 +60,10 @@ server.post("/participants", async (req, res) => {
         await db.collection('users').insertOne({ ...user, lastStatus: Date.now() });
         res.send("Cadastrado");
         await db.collection("messages").insertOne({
-            from: user.name, 
-            to: 'Todos', 
-            text: 'entra na sala...', 
-            type: 'status', 
+            from: user.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
             time: dayjs().format('HH:mm:ss')
         });
         res.sendStatus(201);
@@ -71,6 +78,32 @@ server.get("/participants", async (req, res) => {
         res.send(usersList);
     } catch (error) {
         return res.status(401).send("Não foi possível encontrar os participantes");
+    }
+});
+
+server.post("/messages", async (req, res) => {
+    const message = req.body;
+    const from = req.headers.user;
+    const validation = messageSchema.validate(message, { abortEarly: true });
+    if (validation.error) {
+        return res.status(422).send("Verifique os campos", validation.error);
+    }
+    try {
+        //VERIFICAR SE O PARTICIPANTE EXISTE ANTES DO ENVIO DA MENSSAGEM
+        const verificaUser = await db.collection('users').findOne(from);
+        console.log(verificaUser)
+        if (!verificaUser) {
+            return res.status(422).send("Você não está na sala! Verifique a conexão");
+        }
+        const messageToServer = {
+            ... message, 
+            from,
+            time: dayjs().format('HH:mm:ss')
+        }
+        await db.collection("messages").insertOne(messageToServer);
+        res.sendStatus(201);
+    } catch (error) {
+        return res.status(422).send("Verifique a conexão");
     }
 })
 
